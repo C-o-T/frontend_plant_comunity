c2b7c84ccf3fd7ab84f410b8f817d24bdc8dd5ff
C-o-T <wptmd7789@gmail.com>
2025-09-17 16:15:06 +0900

    main, board

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -1,20 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import styles from './Main.module.css'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Main = () => {
-  //게시글 조회를 위한 state 변수
-  const [boardList, setBoardList] = useState([]);
+  const nav = useNavigate();
 
   //인기글 조회를 위한 state 변수
   const [popularBoardList, setPopularBoardList] = useState([]);
 
   useEffect(() => {
-    axios.get()
-    .then(res => setBoardList(res.data))
-    .catch(e => console.log(e))
-
-    axios.get()
+    axios.get('/api/boards')
     .then(res => setPopularBoardList(res.data))
     .catch(e => console.log(e))
 
@@ -29,7 +25,45 @@ const Main = () => {
         <div>정보</div>
       </div>
       <div>
-        인기글
+        <h2>인기글</h2>
+        <table>
+          <thead>
+            <tr>
+              <td>글 번호</td>
+              <td>제목</td>
+              <td>작성자</td>
+              <td>작성일</td>
+              <td>조회수</td>
+              <td>좋아요</td>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              popularBoardList.length === 0
+              ?
+              <tr>
+                <td colSpan={6}>
+                  작성된 게시글이 없습니다.
+                </td>
+              </tr>
+              :
+              popularBoardList.map((write, i) => {
+                return (
+                  <tr key={i}
+                    onClick={e => nav(`/board/${write.boardNum}`)}
+                  >
+                    <td>{write.boardNum}</td>
+                    <td>{write.title}</td>
+                    <td>{write.memId}</td>
+                    <td>{write.createDate}</td>
+                    <td>{write.readCnt}</td>
+                    <td>{write.likeCnt}</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
       </div>
     </div>
   )
